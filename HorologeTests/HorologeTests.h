d9a6eeff9bd5c89d821ed9b35463183833b322ef
Paul Schifferer <paul@schifferers.net>
2011-06-13 20:22:13 -0700

    Initial Commit

---
+++ HorologeTests/HorologeTests.h
@@ -0,0 +1,13 @@
+//
+//  HorologeTests.h
+//  HorologeTests
+//
+//  Created by Paul Schifferer on 6/13/11.
+//  Copyright 2011 Pilgrimage Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HorologeTests : SenTestCase
+
+@end
---
+++ HorologeTests/HorologeTests.m
@@ -0,0 +1,32 @@
+//
+//  HorologeTests.m
+//  HorologeTests
+//
+//  Created by Paul Schifferer on 6/13/11.
+//  Copyright 2011 Pilgrimage Software. All rights reserved.
+//
+
+#import "HorologeTests.h"
+
+@implementation HorologeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HorologeTests");
+}
+
+@end
